98498cc3da0f1495a299d97e8bd68fd08461beb2
JeamsChang <c.h.jeams@outlook.com>
2023-06-06 00:28:23 +1000

    decode id token

--- html/javascript/js.js
+++ html/javascript/js.js
@@ -1,8 +1,10 @@
 // Parse the URL fragment to get the ID token
 const hashFragment = window.location.hash.substr(1);
 const idToken = new URLSearchParams(hashFragment).get('id_token');
-localStorage.setItem('idToken', idToken);
+const user_id = decodeIdToken(idToken).sub;
+console.log(user_id);
 
+// Convert image to base64
 async function convertImageToBase64(image) {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
@@ -12,6 +14,7 @@ async function convertImageToBase64(image) {
     });
 }
 
+// Upload image to S3
 async function uploadImage(event) {
     event.preventDefault();
     const image = document.querySelector('#file').files[0];
@@ -19,7 +22,7 @@ async function uploadImage(event) {
     const payload = {
         name: image.name,
         file: base64Str,
-        user_id:"b09eef4e-f8d1-4d52-b0d3-e04b56105190"
+        user_id: user_id
     };
     try {
         const response = await fetch("https://rhnlx9ogtj.execute-api.us-east-1.amazonaws.com/pd/image", {
@@ -40,6 +43,21 @@ async function uploadImage(event) {
         var result = "<h2>Upload Failed!</h2><p>" + error.message + "</p>";
         $('#result').html(result);
     }
+}
+
+// Decode the ID token
+function decodeIdToken(token) {
+    let base64Url = token.split('.')[1];
+    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    let jsonPayload = decodeURIComponent(
+        atob(base64)
+        .split('')
+        .map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join(''));
+
+    return JSON.parse(jsonPayload);
 }
 
 // Add the event listener to the form
